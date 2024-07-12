9b2c853ba42ccf65d5a9526765a98af2981b5f99
AmanAsharma070320 <asharma070320@gmail.com>
2024-07-12 17:35:05 +0530

    Github Finder

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+//! COMPONENTS
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+
+//! PAGES
+import HomePage from "./Pages/HomePage";
+import Profile from "./Pages/Profile";
+import NotFound from "./Pages/NotFound";
+// import MyProfile from "./Pages/MyProfile";
+
+//! CONTEXT
+import { ThemeProvider } from "./Context/ThemeContext";
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col justify-between h-screen">
+      <ThemeProvider>
+        <Router>
+          <Header />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            {/* <Route path="/myprofile" element={<MyProfile />} /> */}
+            <Route path="/404" element={<NotFound />} />
+            <Route path="/:username" element={<Profile />} />
+            <Route path="/*" element={<NotFound />} />
+          </Routes>
+          <Footer />
+        </Router>
+      </ThemeProvider>
     </div>
   );
 }
